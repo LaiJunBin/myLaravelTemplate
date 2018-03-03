55d59014e0a0d9436077cc993d8958a721ca1b11
xyz607xx <xyz607xx@gmail.com>
2018-03-04 00:48:48 +0800

    add sign-in/out process

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Validator;
 use App\RegisterUser;
+Use App\User;
 use Hash;
 Use App\Jobs\SendSignUpMailJob;
+
 
 class UserController extends Controller
 {
@@ -82,6 +84,67 @@ class UserController extends Controller
     }
 
     public function signIn(){
-        return 123;
+        $binding = [
+            'navMenu' => [
+                ['url'=>'user/sign-in','title'=>'登入'],
+                ['url'=>'user/sign-up','title'=>'註冊']
+            ],
+            'breadcrumb' =>[
+                ['url'=>'user','title'=>'MyHome'],
+                '登入'
+            ]
+        ];
+        return view('user.signIn',$binding);
     }
+
+    public function signInProcess(){
+        $input = request()->all();
+        
+        $rules = [
+            'email'=>[
+                'required',
+                'max:150',
+                'email'
+            ],
+            'password'=>[
+                'required',
+                'min:6',
+                'max:191'
+            ],
+        ];
+        
+        $validator = Validator::make($input,$rules);
+
+        if($validator->fails()){
+            return redirect('/user/sign-in')->withErrors($validator)->withInput();
+        }
+        
+        $query = User::where(['email' => $input['email']])->first();
+        if($query != null){
+            $is_password_correct = Hash::check($input['password'],$query->password);
+            
+            if($is_password_correct){
+                session()->put('user_name',$query->name);
+                session()->put('user_email',$query->email);
+                return redirect('user');
+            }else{
+                return redirect('user/sign-in')->withErrors('密碼錯誤!')->withInput();
+            }
+        }else{
+            $query = RegisterUser::where('email',$input['email'])->first();
+            if($query != null){
+                return redirect('user/sign-in')->withErrors('這個帳戶還沒被驗證，請到信箱收驗證信!')->withInput();
+            }else{
+                return redirect('user/sign-in')->withErrors('帳戶不存在')->withInput();
+            }
+        }
+        
+    }
+
+    public function signOut(){
+        session()->forget('user_name');
+        session()->forget('user_email');
+        return redirect('user');
+    }
+
 }
